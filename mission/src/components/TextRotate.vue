3d8a235758c72f947df19dd9e361b0e813c19ab0
yeanjae0312 <yeanjae@naver.com>
2022-01-05 20:15:04 +0900

    add real-time-input event

--- mission/src/components/TextRotate.vue
+++ mission/src/components/TextRotate.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="wrap-text-rotate">
     <div class="box-form">
-      <input type="text">
+      <!-- v-model속성 사용 -->
+      <!-- <input type="text" v-model="inputText" placeholder="텍스트를 입력해보세요."> -->
+      <!-- input 속성 사용 -->
+      <input type="text" v-on:input="typingInput" v-bind:value="inputText"
+       placeholder="텍스트를 입력해보세요.">
       <button>제출</button>
       <button>알림</button>
     </div>
-    <p></p>
+    <p class="show-input-text">{{ inputText }}</p>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      inputText: '',
+    };
+  },
+  methods: {
+    typingInput(e) {
+      this.inputText = e.target.value;
+    },
+  },
 };
 </script>
 
@@ -22,5 +35,4 @@ export default {
     align-items: center;
     gap:10px;
   }
-
 </style>
