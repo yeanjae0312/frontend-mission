ee15498f605e807c554f84604ea56bf2f669c9c2
yeanjae0312 <yeanjae@naver.com>
2022-01-05 17:13:14 +0900

    add text-rotate components & style

---
+++ mission/src/components/TextRotate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrap-text-rotate">
+    <div class="box-form">
+      <input type="text">
+      <button>제출</button>
+      <button>알림</button>
+    </div>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+  .wrap-text-rotate .box-form {
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    gap:10px;
+  }
+
+</style>
