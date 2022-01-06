41ed214ff7a59d92f1a9e7ea1900bdb588b05c81
yeanjae0312 <yeanjae@naver.com>
2022-01-06 14:45:03 +0900

    FEAT: 텍스트 회전기능 추가

--- mission/src/components/TextRotate.vue
+++ mission/src/components/TextRotate.vue
@@ -4,9 +4,9 @@
       <!-- v-model속성 사용 -->
       <!-- <input type="text" v-model="inputText" placeholder="텍스트를 입력해보세요."> -->
       <!-- input 속성 사용 -->
-      <input type="text" v-on:input="typingInput" v-bind:value="inputText"
+      <input type="text" @input="typingInput" v-bind:value="inputText"
        placeholder="텍스트를 입력해보세요.">
-      <button>제출</button>
+      <button class="btn-rotate" @click="rotateText">회전</button>
       <button>알림</button>
     </div>
     <p class="show-input-text">{{ inputText }}</p>
@@ -23,6 +23,10 @@ export default {
   methods: {
     typingInput(e) {
       this.inputText = e.target.value;
+    },
+    rotateText() {
+      // 두번재 요소부터 텍스트를 남겨놓고 0번째부터 1번째 요소까지(0을 인덱스로 하는 요소)의 텍스트를 뒤에 붙여준다.
+      this.inputText = this.inputText.slice(1) + this.inputText.slice(0, 1);
     },
   },
 };
