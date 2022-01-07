28c2c54406cd4fe193975107919f5d972bb6c7f0
yeanjae0312 <yeanjae@naver.com>
2022-01-07 15:57:05 +0900

    CHORE: lint rules 수정

--- mission/.eslintrc.js
+++ mission/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': ['error', require('os').EOL === '\r\n' ? 'windows' : 'unix'],
+    'max-len': 'off',
   },
   overrides: [
     {
