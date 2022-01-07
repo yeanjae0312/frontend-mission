19398d49faf3aeaeb14e8c9e701173da366f5481
yeanjae0312 <yeanjae@naver.com>
2022-01-07 15:58:13 +0900

    TEST: 테스트 코드 수정 & 모달관련 테스트 코드 추가

--- mission/tests/unit/textRotateTest.spec.js
+++ mission/tests/unit/textRotateTest.spec.js
@@ -1,32 +1,53 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils';
 import TextRotate from '@/components/TextRotate.vue';
 
 describe('TextRotate.vue', () => {
-  const wrapper = shallowMount(TextRotate);
-  const input = wrapper.find('input');
-  const showArea = wrapper.find('.show-input-text');
-
-  test('input값을 입력하면 아래에 입력한 내용이 보인다.', async () => {
-    await input.setValue('input value');
-
-    expect(showArea.text()).toBe('input value');
-  });
-
-  test('회전 버튼 클릭시 아래에 글자가 회전된다.', async () => {
-    const rotateBtn = wrapper.find('.btn-rotate');
+  test('회전 버튼 클릭시 글자가 회전된다.', async () => {
+    const wrapper = shallowMount(TextRotate);
+    const input = wrapper.find('input');
+    const btn = wrapper.find('button');
 
     await input.setValue('Projectlion');
-    await rotateBtn.trigger('click');
+    await btn.trigger('click');
 
-    expect(showArea.text()).toBe('rojectlionP');
+    expect(wrapper.text()).toContain('rojectlionP');
   });
 
-  test('회전 버튼 클릭시 아래에 숫자가 회전된다.', async () => {
-    const rotateBtn = wrapper.find('.btn-rotate');
+  test('회전 버튼 클릭시 숫자가 회전된다.', async () => {
+    const wrapper = shallowMount(TextRotate);
+    const input = wrapper.find('input');
+    const btn = wrapper.find('button');
 
     await input.setValue('12345');
-    await rotateBtn.trigger('click');
+    await btn.trigger('click');
+    await btn.trigger('click');
 
-    expect(showArea.text()).toBe('23451');
+    expect(wrapper.text()).toContain('34512');
   });
+
+  /* test('클릭수 버튼 클릭시 input에 입력된 문자열을 모달로 보여준다.', async () => {
+    const wrapper = mount(TextRotate);
+    const input = wrapper.find('input');
+    const btn = wrapper.find('button');
+    const modal = wrapper.find('#modal');
+    
+    await input.setValue('Projectlion');
+    await btn.trigger('click');
+
+    expect(modal.text()).toContain('Projectlion');
+  }); */
+
+  /* test('클릭수 버튼 클릭시 클릭 수를 모달로 보여준다.', async () => {
+    const wrapper = mount(TextRotate);
+    const btn = wrapper.find('button');
+    const modal = wrapper.find('#modal');
+
+    await btn.trigger('click');
+    await btn.trigger('click');
+    await btn.trigger('click');
+    await btn.trigger('click');
+    await btn.trigger('click');
+
+    expect(modal.text()).toContain('5');
+  }); */
 });
