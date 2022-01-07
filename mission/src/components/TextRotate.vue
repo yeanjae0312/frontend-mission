8767ad942737e346e29905a8e94603680be99c0d
yeanjae0312 <yeanjae@naver.com>
2022-01-07 15:59:18 +0900

    FEAT: 알림창 띄우기 기능 추가

--- mission/src/components/TextRotate.vue
+++ mission/src/components/TextRotate.vue
@@ -4,20 +4,25 @@
       <!-- v-model속성 사용 -->
       <!-- <input type="text" v-model="inputText" placeholder="텍스트를 입력해보세요."> -->
       <!-- input 속성 사용 -->
-      <input type="text" @input="typingInput" v-bind:value="inputText"
-       placeholder="텍스트를 입력해보세요.">
+      <input type="text" @input="typingInput" v-bind:value="inputText" placeholder="텍스트를 입력해보세요.">
       <button class="btn-rotate" @click="rotateText">회전</button>
-      <button>알림</button>
+      <button class="btn-count" @click="countClick">알림</button>
     </div>
     <p class="show-input-text">{{ inputText }}</p>
+
+    <TextRotateModal v-if="showModal" @close="showModal = false" id="modal" v-bind:propsdata="{inputText, countNum}"></TextRotateModal>
   </div>
 </template>
 
 <script>
+import TextRotateModal from './TextRotateModal.vue';
+
 export default {
   data() {
     return {
       inputText: '',
+      showModal: false,
+      countNum: 0,
     };
   },
   methods: {
@@ -28,6 +33,13 @@ export default {
       // 두번재 요소부터 텍스트를 남겨놓고 0번째부터 1번째 요소까지(0을 인덱스로 하는 요소)의 텍스트를 뒤에 붙여준다.
       this.inputText = this.inputText.slice(1) + this.inputText.slice(0, 1);
     },
+    countClick() {
+      this.countNum += 1;
+      this.showModal = true;
+    },
+  },
+  components: {
+    TextRotateModal,
   },
 };
 </script>
---
+++ mission/src/components/TextRotateModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Modal class="hihi">
+      <template v-slot:header>
+        <h3>입력값과 클릭 수 확인</h3>
+      </template>
+      <template v-slot:body>
+        <div>{{ inputText }} : {{ countNum }}</div>
+      </template>
+      <template v-slot:footer>
+        <button class="modal-default-button" @click="$emit('close')">Close</button>
+      </template>
+    </Modal>
+  </div>
+</template>
+
+<script>
+import Modal from './common/Modal.vue';
+
+export default {
+  props: ['propsdata'],
+  data() {
+    return {
+      inputText: this.propsdata.inputText,
+      countNum: this.propsdata.countNum,
+    };
+  },
+  components: {
+    Modal,
+  },
+};
+</script>
+
+<style>
+
+</style>
