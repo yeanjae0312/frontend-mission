15aed5b8fcc1d053c70ecfeef632e708ac188958
yeanjae0312 <yeanjae@naver.com>
2022-01-08 23:35:11 +0900

    STYLE: 모달 radius변경

--- mission/src/components/common/Modal.vue
+++ mission/src/components/common/Modal.vue
@@ -61,7 +61,7 @@ export default {
   margin: 0px auto;
   padding: 20px 30px;
   background-color: #fff;
-  border-radius: 2px;
+  border-radius: 8px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
   transition: all 0.3s ease;
   font-family: Helvetica, Arial, sans-serif;
