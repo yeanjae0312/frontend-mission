b8263e5d08ac07cf1c17caa2a3736e9d3a4fb2e8
yeanjae0312 <yeanjae@naver.com>
2022-01-12 16:33:35 +0900

    ADD: 데이터 세팅

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -7,7 +7,37 @@ export default {
   name: 'ItemInfoPage',
   data() {
     return {
-
+      company: {
+        name: '멋쟁이사자처럼',
+        /* eslint-disable global-require */
+        profile: require('../assets/img/company-img.jpg'),
+        tag: ['시크', '심플베이직', '페미닌', '러블리', '섹시글램', '유니크', '캐쥬얼'],
+        book: false,
+      },
+      product: {
+        name: '[누빔안감] 핸드메이드 울 롱코트',
+        /* eslint-disable global-require */
+        img: require('../assets/img/product-img.jpg'),
+        discountRate: '39',
+        price: 98000,
+        detail: '<img src="/img/product-detail01.jpg"><span style="color: red">This should be red.</span>',
+      },
+      reviewer: [
+        {
+          name: 'lion',
+          title: '만족해요',
+          contentString: '핏이 이쁘고 따뜻해서 만족해요. 요즘 교복처럼 입고다니는 옷이네요~',
+          /* eslint-disable global-require */
+          contentImg: require('../assets/img/review-img01.png'),
+        },
+        {
+          name: 'cuty lion',
+          title: '별로에요',
+          contentString: '생각보다 두껍고 무게가 나가네요. 보풀도 잘 일어날 것 같은 재질이에요.',
+          /* eslint-disable global-require */
+          contentImg: require('../assets/img/review-img02.jpg'),
+        },
+      ],
     };
   },
   methods: {
