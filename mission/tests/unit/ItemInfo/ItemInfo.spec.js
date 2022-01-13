fa9b3096557896aa21de3e06c5c9d24bc4bace1c
yeanjae0312 <yeanjae@naver.com>
2022-01-13 12:35:40 +0900

    STYLE: lint규칙에 맞게 테스트코드 포맷팅

--- mission/tests/unit/ItemInfo/ItemInfo.spec.js
+++ mission/tests/unit/ItemInfo/ItemInfo.spec.js
@@ -8,75 +8,75 @@ describe('ItemInfoPage', () => {
     expect(wrapper.find('#item-info-page').exists()).toBe(true);
   });
 
-  it('상품 이미지 영역이 있는가', async() => {
+  it('상품 이미지 영역이 있는가', async () => {
     const wrapper = mount(ItemInfoPage);
 
     expect(wrapper.get('[data-test="product-img"]').exists()).toBe(true);
-  })
+  });
 
-  it('판매자 정보에 프로필 영역이 있는가', async() => {
+  it('판매자 정보에 프로필 영역이 있는가', async () => {
     const wrapper = mount(ItemInfoPage);
 
     expect(wrapper.get('[data-test="company-profile"]').exists()).toBe(true);
-  })
+  });
 
-  it('판매자 정보에 상호명과 태그 영역이 있는가', async() => {
+  it('판매자 정보에 상호명과 태그 영역이 있는가', async () => {
     const wrapper = mount(ItemInfoPage);
 
     expect(wrapper.get('[data-test="company-info"]').exists()).toBe(true);
-  })
+  });
 
-  it('판매자 정보에 즐겨찾기 영역이 있는가', async() => {
+  it('판매자 정보에 즐겨찾기 영역이 있는가', async () => {
     const wrapper = mount(ItemInfoPage);
 
     expect(wrapper.get('[data-test="company-star"]').exists()).toBe(true);
-  })
+  });
 
-  it('상호명에 데이터 값이 잘 들어갔는가', async() => {
+  it('상호명에 데이터 값이 잘 들어갔는가', async () => {
     const wrapper = mount(ItemInfoPage);
 
     await wrapper.setData({
       company: {
         name: '에이블리',
-      }
+      },
     });
 
     expect(wrapper.get('[data-test="company-info-name"]').text()).toBe('에이블리');
-  })
+  });
 
-  it('태그 리스트에 데이터 값이 잘 들어갔는가', async() => {
+  it('태그 리스트에 데이터 값이 잘 들어갔는가', async () => {
     const wrapper = mount(ItemInfoPage);
 
     await wrapper.setData({
       company: {
         tag: ['심플'],
-      }
+      },
     });
 
     expect(wrapper.get('[data-test="company-info-tag"]').text()).toContain('심플');
-  })
+  });
 
-  it('즐겨찾기 버튼 한번 클릭시 즐겨찾기가 등록이 되는가', async() => {
+  it('즐겨찾기 버튼 한번 클릭시 즐겨찾기가 등록이 되는가', async () => {
     const wrapper = mount(ItemInfoPage);
 
     await wrapper.setData({
       company: {
         star: true,
-      }
+      },
     });
 
     expect(wrapper.get('[data-test="company-star"]').find('i.fas').isVisible()).toBe(true);
-  })
+  });
 
-  it('선택되어 있는 즐겨찾기 버튼 클릭시 즐겨찾기 해제가 되는가', async() => {
+  it('선택되어 있는 즐겨찾기 버튼 클릭시 즐겨찾기 해제가 되는가', async () => {
     const wrapper = mount(ItemInfoPage);
 
     await wrapper.setData({
       company: {
         star: false,
-      }
+      },
     });
 
     expect(wrapper.get('[data-test="company-star"]').find('i.far').isVisible()).toBe(true);
-  })
+  });
 });
