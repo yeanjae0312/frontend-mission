a1e334b2f596845fb70f794bc7063d90d0c987c9
yeanjae0312 <yeanjae@naver.com>
2022-01-18 15:13:43 +0900

    TEST: TC 리팩토링

--- mission/tests/unit/ItemInfo/ItemInfo.spec.js
+++ mission/tests/unit/ItemInfo/ItemInfo.spec.js
@@ -2,39 +2,33 @@ import { mount } from '@vue/test-utils';
 import ItemInfoPage from '@/views/ItemInfo.vue';
 
 describe('ItemInfoPage', () => {
-  it('redners ItemInfoPage', () => {
-    const wrapper = mount(ItemInfoPage);
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = mount(ItemInfoPage);
+  });
+
+  it('redners ItemInfoPage', () => {
     expect(wrapper.find('#item-info-page').exists()).toBe(true);
   });
 
   it('상품 이미지 영역이 있는가', () => {
-    const wrapper = mount(ItemInfoPage);
-
     expect(wrapper.get('[data-test="product-img"]').exists()).toBe(true);
   });
 
   it('판매자 정보에 프로필 영역이 있는가', () => {
-    const wrapper = mount(ItemInfoPage);
-
     expect(wrapper.get('[data-test="company-profile"]').exists()).toBe(true);
   });
 
   it('판매자 정보에 상호명과 태그 영역이 있는가', () => {
-    const wrapper = mount(ItemInfoPage);
-
     expect(wrapper.get('[data-test="company-info"]').exists()).toBe(true);
   });
 
   it('판매자 정보에 즐겨찾기 영역이 있는가', () => {
-    const wrapper = mount(ItemInfoPage);
-
     expect(wrapper.get('[data-test="company-star"]').exists()).toBe(true);
   });
 
   it('상호명에 데이터 값이 잘 들어갔는가', async () => {
-    const wrapper = mount(ItemInfoPage);
-
     await wrapper.setData({
       company: {
         name: '에이블리',
@@ -45,8 +39,6 @@ describe('ItemInfoPage', () => {
   });
 
   it('태그 리스트에 데이터 값이 잘 들어갔는가', async () => {
-    const wrapper = mount(ItemInfoPage);
-
     await wrapper.setData({
       company: {
         tag: ['심플'],
@@ -56,38 +48,24 @@ describe('ItemInfoPage', () => {
     expect(wrapper.get('[data-test="company-info-tag"]').text()).toContain('심플');
   });
 
-  it('즐겨찾기 버튼 한번 클릭시 즐겨찾기가 등록이 되는가', async () => {
-    const wrapper = mount(ItemInfoPage);
+  it('즐겨찾기 클릭시 토글기능이 동작 하는가', async () => {
+    const starBtn = wrapper.get('[data-test="company-star-solid"]');
 
-    await wrapper.setData({
-      company: {
-        star: true,
-      },
-    });
+    await starBtn.trigger('click');
 
-    expect(wrapper.get('[data-test="company-star"]').find('i.fas').isVisible()).toBe(true);
-  });
+    expect(wrapper.get('[data-test="company-star-fill"]').isVisible()).toBe(true);
 
-  it('선택되어 있는 즐겨찾기 버튼 클릭시 즐겨찾기 해제가 되는가', async () => {
-    const wrapper = mount(ItemInfoPage);
+    await wrapper.get('[data-test="company-star-fill"]').trigger('click');
 
-    await wrapper.setData({
-      company: {
-        star: false,
-      },
-    });
-
-    expect(wrapper.get('[data-test="company-star"]').find('i.far').isVisible()).toBe(true);
+    expect(wrapper.get('[data-test="company-star-solid"]').isVisible()).toBe(true);
   });
 
   it('상품의 기본정보 영역이 있는가', () => {
-    const wrapper = mount(ItemInfoPage);
-
     expect(wrapper.get('[data-test="product-info"]').exists()).toBe(true);
   });
 
   it('할인율와 정가를 입력하면 할인가가 계산되어 나오는가', async () => {
-    const wrapper = mount(ItemInfoPage);
+    const resultDiscountedPrice = 50000;
 
     await wrapper.setData({
       product: {
@@ -96,24 +74,18 @@ describe('ItemInfoPage', () => {
       },
     });
 
-    expect(wrapper.get('[data-test="discount-num"]').text()).toContain('50000');
+    expect(wrapper.get('[data-test="discount-num"]').text()).toContain(resultDiscountedPrice);
   });
 
   it('상품 상세 설명 영역이 있는가', () => {
-    const wrapper = mount(ItemInfoPage);
-
     expect(wrapper.get('[data-test="product-detail"]').exists()).toBe(true);
   });
 
   it('리뷰 영역이 있는가', () => {
-    const wrapper = mount(ItemInfoPage);
-
     expect(wrapper.get('[data-test="review"]').exists()).toBe(true);
   });
 
   it('리뷰어 이름이 잘 가져와 지는가', async () => {
-    const wrapper = mount(ItemInfoPage);
-
     await wrapper.setData({
       reviewer: [
         {
@@ -126,8 +98,6 @@ describe('ItemInfoPage', () => {
   });
 
   it('리뷰 날짜가 잘 가져와 지는가', async () => {
-    const wrapper = mount(ItemInfoPage);
-
     await wrapper.setData({
       reviewer: [
         {
@@ -140,8 +110,6 @@ describe('ItemInfoPage', () => {
   });
 
   it('리뷰 제목이 잘 가져와 지는가', async () => {
-    const wrapper = mount(ItemInfoPage);
-
     await wrapper.setData({
       reviewer: [
         {
@@ -154,8 +122,6 @@ describe('ItemInfoPage', () => {
   });
 
   it('리뷰 본문이 잘 가져와 지는가', async () => {
-    const wrapper = mount(ItemInfoPage);
-
     await wrapper.setData({
       reviewer: [
         {
@@ -168,14 +134,10 @@ describe('ItemInfoPage', () => {
   });
 
   it('구매 버튼 영역이 있는가', () => {
-    const wrapper = mount(ItemInfoPage);
-
     expect(wrapper.get('[data-test="btn-purchase"]').exists()).toBe(true);
   });
 
   it('구매 버튼 안에 가격의 데이터가 잘 가져와 지는가', async () => {
-    const wrapper = mount(ItemInfoPage);
-
     await wrapper.setData({
       product: {
         discountRate: '50',
@@ -186,15 +148,15 @@ describe('ItemInfoPage', () => {
     expect(wrapper.get('[data-test="btn-purchase"]').text()).toContain('50000');
   });
 
-  it('찜하기 버튼을 클릭하면 등록이 되는가', async () => {
-    const wrapper = mount(ItemInfoPage);
+  it('찜하기 클릭시 토글기능이 동작 하는가', async () => {
+    const heartBtn = wrapper.get('[data-test="heart-solid"]');
 
-    await wrapper.setData({
-      product: {
-        heart: true,
-      },
-    });
+    await heartBtn.trigger('click');
 
-    expect(wrapper.get('[data-test="heart"]').find('i.fas').isVisible()).toBe(true);
+    expect(wrapper.get('[data-test="heart-fill"]').isVisible()).toBe(true);
+
+    await wrapper.get('[data-test="heart-fill"]').trigger('click');
+
+    expect(wrapper.get('[data-test="heart-solid"]').isVisible()).toBe(true);
   });
 });
