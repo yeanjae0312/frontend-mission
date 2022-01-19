0c4134fd7b83d051f0f24a83f6ae38267416a9e6
yeanjae0312 <yeanjae@naver.com>
2022-01-19 14:39:51 +0900

    TEST: 컴포넌트들이 있는지 TC 추가

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -2,9 +2,25 @@ import { mount } from '@vue/test-utils';
 import ItemInfoPage from '@/views/ItemList.vue';
 
 describe('ItemListPage', () => {
-  it('redners ItemListPage', () => {
-    const wrapper = mount(ItemInfoPage);
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = mount(ItemInfoPage);
+  });
+
+  it('redners ItemListPage', () => {
     expect(wrapper.find('#item-list-page').exists()).toBe(true);
+  });
+
+  it('헤더 컴포넌트가 있는가', () => {
+    expect(wrapper.get('[data-test="header"]').exists()).toBe(true);
+  });
+
+  it('아이템 컴포넌트가 있는가', () => {
+    expect(wrapper.get('[data-test="item"]').exists()).toBe(true);
+  });
+
+  it('하단 내비게이션 컴포넌트가 있는가', () => {
+    expect(wrapper.get('[data-test="navBar"]').exists()).toBe(true);
   });
 });
