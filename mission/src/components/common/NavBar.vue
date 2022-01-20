785294fb415bc32ffd354ec5615ab8205c61118c
yeanjae0312 <yeanjae@naver.com>
2022-01-20 13:37:46 +0900

    FEAT: Nav 구현

--- mission/src/components/common/NavBar.vue
+++ mission/src/components/common/NavBar.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="nav">nav</div>
+  <div class="nav">
+    <div class="nav-inner">
+      <div class="nav-item">
+        <a href="#none">
+          <font-awesome-icon data-test="nav-icon-home" :icon="['fas','home']" class="item-icon" />
+          <p data-test="nav-text-home" class="item-conts">홈</p>
+        </a>
+      </div>
+
+      <div class="nav-item">
+        <a href="#none">
+          <font-awesome-icon data-test="nav-icon-heart" :icon="['fas','heart']" class="item-icon" />
+          <p data-test="nav-text-heart" class="item-conts">찜</p>
+        </a>
+      </div>
+
+      <div class="nav-item">
+        <a href="#none">
+          <font-awesome-icon data-test="nav-icon-cart" :icon="['fas','shopping-cart']" class="item-icon" />
+          <p data-test="nav-text-cart" class="item-conts">장바구니</p>
+        </a>
+      </div>
+
+      <div class="nav-item">
+        <a href="#none">
+          <font-awesome-icon data-test="nav-icon-my" :icon="['fas','user']" class="item-icon" />
+          <p data-test="nav-text-my" class="item-conts">My</p>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +38,43 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+  .nav {
+    position: fixed;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
 
+    width: 100%;
+    max-width: var(--max-size);
+    border-top: solid 1px rgb(192, 192, 192);
+    box-sizing: border-box;
+    box-shadow: 0px -4px 4px -4px rgb(141, 141, 141);
+
+  }
+
+  .nav-inner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+
+    padding: 10px var(--paddingSide);
+    box-sizing: border-box;
+  }
+
+  .nav-inner .nav-item {
+    width: calc(100% / 4);
+    text-align: center;
+  }
+
+  .nav-inner .nav-item a {
+    padding: 0 4px;
+    display: block;
+  }
+
+  .nav-inner .nav-item .item-conts {
+    margin-top: 2px;
+    font-size: 12px;
+  }
 </style>
