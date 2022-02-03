9df27c2354ce1ae96629c171736cc22dcf8f998c
yeanjae0312 <yeanjae@naver.com>
2022-02-03 18:58:09 +0900

    TEST: ItemInfo API연동으로 인한 TC 수정

--- mission/tests/unit/ItemInfo/ItemInfo.spec.js
+++ mission/tests/unit/ItemInfo/ItemInfo.spec.js
@@ -19,7 +19,7 @@ describe('ItemInfoPage', () => {
   });
 
   it('redners ItemInfoPage', () => {
-    expect(wrapper.find('#item-info-page').exists()).toBe(true);
+    expect(wrapper.find('.item-info-page').exists()).toBe(true);
   });
 
   it('상품 이미지 영역이 있는가', () => {
@@ -40,8 +40,10 @@ describe('ItemInfoPage', () => {
 
   it('상호명에 데이터 값이 잘 들어갔는가', async () => {
     await wrapper.setData({
-      company: {
-        name: '에이블리',
+      item: {
+        seller: {
+          name: '에이블리',
+        },
       },
     });
 
@@ -50,41 +52,29 @@ describe('ItemInfoPage', () => {
 
   it('태그 리스트에 데이터 값이 잘 들어갔는가', async () => {
     await wrapper.setData({
-      company: {
-        tag: ['심플'],
+      item: {
+        seller: {
+          hash_tags: ['심플'],
+        },
       },
     });
 
     expect(wrapper.get('[data-test="company-info-tag"]').text()).toContain('심플');
   });
 
-  it('즐겨찾기 클릭시 토글기능이 동작 하는가', async () => {
-    const starBtn = wrapper.get('[data-test="company-star-solid"]');
-
-    await starBtn.trigger('click');
-
-    expect(wrapper.get('[data-test="company-star-fill"]').isVisible()).toBe(true);
-
-    await wrapper.get('[data-test="company-star-fill"]').trigger('click');
-
-    expect(wrapper.get('[data-test="company-star-solid"]').isVisible()).toBe(true);
-  });
-
   it('상품의 기본정보 영역이 있는가', () => {
     expect(wrapper.get('[data-test="product-info"]').exists()).toBe(true);
   });
 
-  it('할인율와 정가를 입력하면 할인가가 계산되어 나오는가', async () => {
-    const resultDiscountedPrice = 50000;
-
+  it('판매가와 원가를 입력시 할인율이 잘 도출되는가', async () => {
     await wrapper.setData({
-      product: {
-        discountRate: '50',
-        price: 100000,
+      item: {
+        price: 50000,
+        original_price: 100000,
       },
     });
 
-    expect(wrapper.get('[data-test="discount-num"]').text()).toContain(resultDiscountedPrice);
+    expect(wrapper.get('[data-test="discount-num"]').text()).toContain('50');
   });
 
   it('상품 상세 설명 영역이 있는가', () => {
@@ -97,11 +87,13 @@ describe('ItemInfoPage', () => {
 
   it('리뷰어 이름이 잘 가져와 지는가', async () => {
     await wrapper.setData({
-      reviewer: [
-        {
-          name: 'hello',
-        },
-      ],
+      item: {
+        reviews: [
+          {
+            writer: 'hello',
+          },
+        ],
+      },
     });
 
     expect(wrapper.get('[data-test="reviewer-name"]').text()).toContain('hello');
@@ -109,11 +101,13 @@ describe('ItemInfoPage', () => {
 
   it('리뷰 날짜가 잘 가져와 지는가', async () => {
     await wrapper.setData({
-      reviewer: [
-        {
-          date: '2021. 04. 29',
-        },
-      ],
+      item: {
+        reviews: [
+          {
+            created: '2021. 04. 29',
+          },
+        ],
+      },
     });
 
     expect(wrapper.get('[data-test="reviewer-date"]').text()).toContain('2021. 04. 29');
@@ -121,11 +115,13 @@ describe('ItemInfoPage', () => {
 
   it('리뷰 제목이 잘 가져와 지는가', async () => {
     await wrapper.setData({
-      reviewer: [
-        {
-          title: '리뷰 타이틀',
-        },
-      ],
+      item: {
+        reviews: [
+          {
+            title: '리뷰 타이틀',
+          },
+        ],
+      },
     });
 
     expect(wrapper.get('[data-test="reviewer-title"]').text()).toContain('리뷰 타이틀');
@@ -133,11 +129,13 @@ describe('ItemInfoPage', () => {
 
   it('리뷰 본문이 잘 가져와 지는가', async () => {
     await wrapper.setData({
-      reviewer: [
-        {
-          contentString: '리뷰 본문',
-        },
-      ],
+      item: {
+        reviews: [
+          {
+            content: '리뷰 본문',
+          },
+        ],
+      },
     });
 
     expect(wrapper.get('[data-test="reviewer-cont"]').text()).toContain('리뷰 본문');
@@ -149,24 +147,12 @@ describe('ItemInfoPage', () => {
 
   it('구매 버튼 안에 가격의 데이터가 잘 가져와 지는가', async () => {
     await wrapper.setData({
-      product: {
-        discountRate: '50',
+      item: {
         price: 100000,
+        original_price: 140000,
       },
     });
 
-    expect(wrapper.get('[data-test="btn-purchase"]').text()).toContain('50000');
-  });
-
-  it('찜하기 클릭시 토글기능이 동작 하는가', async () => {
-    const heartBtn = wrapper.get('[data-test="heart-solid"]');
-
-    await heartBtn.trigger('click');
-
-    expect(wrapper.get('[data-test="heart-fill"]').isVisible()).toBe(true);
-
-    await wrapper.get('[data-test="heart-fill"]').trigger('click');
-
-    expect(wrapper.get('[data-test="heart-solid"]').isVisible()).toBe(true);
+    expect(wrapper.get('[data-test="btn-purchase"]').text()).toContain('100,000');
   });
 });
